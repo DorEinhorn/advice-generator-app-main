2e90d6b8168cb32b426af3e3efdc91d5f48bb641
Dor Einhorn <136995495+DorEinhorn@users.noreply.github.com>
2024-04-10 17:42:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const adviceNum = document.querySelector('.adviceid');
 const adviceContent = document.querySelector('.advicecontent');
+const dice = document.querySelector('button');
 
 async function adviceId(){
     const response = await fetch("https://api.adviceslip.com/advice");
     const advice = await response.json();
-    console.log(advice);
-    console.log(advice.slip.advice);
     adviceNum.textContent = advice.slip.id;
     adviceContent.textContent = advice.slip.advice;
 }
 
 adviceId();
+
+dice.addEventListener('click', adviceId);
